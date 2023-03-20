4c7a945eb35c11cb68c12da685b3f4d06def5cff
aryan19978 <37269340+aryan19978@users.noreply.github.com>
2023-03-20 23:54:32 +0530

    monday morning backup

--- force-app/main/default/aura/NavigateToBuildQuote/NavigateToBuildQuoteController.js
+++ force-app/main/default/aura/NavigateToBuildQuote/NavigateToBuildQuoteController.js
@@ -1,29 +1,28 @@
 ({    
     
     fetchButtonId : function(component, event, helper) {
+        
+    },
+    
+    invoke : function(component, event, helper) {
         var action = component.get("c.fetchQuoteButtonValue");
         action.setParams({
             LabelValue:"Signed"
         });
         action.setCallback(this, function(response){
+            console.log(response.getReturnValue());
             var state = response.getState();
             if (state === "SUCCESS") {
                 component.set("v.ButtonID", response.getReturnValue());
+                
+                var record = component.get("v.recordId");
+                var urlEvent = $A.get("e.force:navigateToURL");
+                urlEvent.setParams({
+                    "url": "/apex/pdffiller_sfree__airslateApp?buttonId="+component.get("v.ButtonID")+"&recordId="+ record +"&objectName=Quote"
+                });
+                urlEvent.fire(); 
             }
         });
         $A.enqueueAction(action);
-    },
-    
-    invoke : function(component, event, helper) {
-        var record = component.get("v.recordId");
-        
-        var urlEvent = $A.get("e.force:navigateToURL");
-        // urlEvent.setParams({
-        //     "url": "/apex/pdffiller_sfree__airslateApp?buttonId="+component.get("v.ButtonID")+"&recordId="+ record +"&objectName=Quote"
-        // });
-        urlEvent.setParams({
-            "url": "/apex/pdffiller_sfree__airslateApp?buttonId=a0B75000000LQDKEA4&recordId="+ record +"&objectName=Quote"
-        });
-        urlEvent.fire();
     }
 })
