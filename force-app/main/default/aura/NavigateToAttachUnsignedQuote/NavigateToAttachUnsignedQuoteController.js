affcc2b7fd2cf06d1b9a7755ae1eb7747e484ee8
aryan19978 <37269340+aryan19978@users.noreply.github.com>
2023-03-29 10:19:56 +0530

    wednesday morning backup

--- force-app/main/default/aura/NavigateToAttachUnsignedQuote/NavigateToAttachUnsignedQuoteController.js
+++ force-app/main/default/aura/NavigateToAttachUnsignedQuote/NavigateToAttachUnsignedQuoteController.js
@@ -1,26 +1,10 @@
 ({   
-    fetchButtonId : function(component, event, helper) {
-        var action = component.get("c.fetchQuoteButtonValue");
-        action.setParams({
-            LabelValue:"UnSigned"
-        });
-        action.setCallback(this, function(response){
-            var state = response.getState();
-            if (state === "SUCCESS") {
-                component.set("v.ButtonID", response.getReturnValue());
-            }
-        });
-        $A.enqueueAction(action);
-    },
     invoke : function(component, event, helper) {
-        var record = component.get("v.recordId");
-        
-        var urlEvent = $A.get("e.force:navigateToURL");
-        // urlEvent.setParams({
-        //     "url": "/apex/pdffiller_sfree__airslateApp?buttonId="+component.get("v.ButtonID")+"&recordId="+ record +"&objectName=Quote"
-        // });
-        urlEvent.setParams({
-            "url": "/apex/pdffiller_sfree__airslateApp?buttonId=a0B75000000LPxYEAW&recordId="+ record +"&objectName=Quote"
-        });
-        urlEvent.fire();
+    
+                var record = component.get("v.recordId");
+                var urlEvent = $A.get("e.force:navigateToURL");
+                urlEvent.setParams({
+                    "url": "/apex/pdffiller_sfree__airslateApp?buttonId="+$A.get("$Label.c.Un_Signed_Quote_Button_Id")+"&recordId="+ record +"&objectName=Quote"
+                });
+                urlEvent.fire();    
     }})
